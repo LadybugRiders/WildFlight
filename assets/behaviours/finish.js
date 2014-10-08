474cab0417e6b6f8f9b6f339d6213929f08e5f90
Mefteg <gimenez.tom@gmail.com>
2014-10-08 02:14:15 +0200

    Add an effect on the end of a level.

--- assets/behaviours/finish.js
+++ assets/behaviours/finish.js
@@ -13,13 +13,26 @@ Finish.prototype.onBeginContact = function(_otherBody, _myShape, _otherShape, _e
   var pollinator = this.go.game.pollinator;
   if (pollinator) pollinator.dispatch("playerFinishes");
 
-  // create the timer for finish the level
-  this.entity.game.time.events.add(
-    Phaser.Timer.SECOND * 1,
-    function() {
-    	var state = this.go.game.state.getCurrentState();
-	    this.entity.game.state.start("Level", true, false, {levelName: "level_selection"});
-	  },
-    this
-   );
+  var bitmapData = new Phaser.BitmapData(this.go.game, "myBitmap", 16, 16);
+  bitmapData.fill(0, 0, 0, 1);
+  var sprite = this.go.game.add.sprite(0, 0);
+  sprite.anchor.setTo(0.5, 0.5);
+  sprite.fixedToCamera = true;
+  sprite.cameraOffset.x = this.go.game.camera.width * 0.5;
+  sprite.cameraOffset.y = this.go.game.camera.height * 0.5;
+  sprite.alpha = 0.75;
+
+  bitmapData.add(sprite);
+
+  var tween = this.go.game.add.tween(sprite).to({
+      width: this.go.game.camera.width,
+      height: this.go.game.camera.height,
+      alpha: 1
+    }, 1500, Phaser.Easing.Quadratic.Out, true
+  );
+
+  tween.onComplete.add(function() {
+    var state = this.go.game.state.getCurrentState();
+    this.entity.game.state.start("Level", true, false, {levelName: "level_selection"});
+  }, this);
 }
